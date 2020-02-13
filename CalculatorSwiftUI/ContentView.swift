4947732ad5e96ede8859d62132bea35df4d18b1b
Alex Cheung <alexxcheungg@gmail.com>
2020-02-13 15:46:23 +0800

    Initial Commit

---
+++ CalculatorSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CalculatorSwiftUI
+//
+//  Created by Alex Cheung on 13/2/2020.
+//  Copyright © 2020 Alex Cheung. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
